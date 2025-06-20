6e2b1d4fc9dc0f43dec58e9b56814110cbe68da9
Vinitha C <vinithac.23aid@kongu.edu>
2025-06-20 14:26:32 +0530

    Add files via upload

---
+++ collections.java
@@ -0,0 +1,20 @@
+class ArrayFunctions
+{
+public static void main(String[] args)
+{
+ArrayList<String>fruits=new Arraylist<>();
+fruits.add("Apple");
+fruits.add("Mango");
+fruits.add("orange");
+fruits.add("grapes");
+fruits.add("pineapple");
+System.out.println("fruits in the lists are"+fruits);
+fruits.remove("grapes");
+fruits.remove("orange");
+System.out.println("fruits after the deletion are"+fruits);
+}
+}
+
+
+
+
